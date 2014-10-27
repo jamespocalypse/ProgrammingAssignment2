7de5f9ee0eba61d1c29f4faeb6996eb3236bcede
James Robertson <jrober84@gmail.com>
2014-10-26 22:29:08 -0400

    Assignment Number 2 submission
    Contains the assignment 2 data submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
+## Assignment #2 for R programming to demonstrate lexical scoping and caching of variables to speed up costly
+## operations which are run multiple times
 ## functions do
 
-## Write a short comment describing this function
+## Function accepts a matrix and caches its inverse values to be used elsewhere in the script
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## computes the imverse square of a caches matrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("Retrieving matrix data cache")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
